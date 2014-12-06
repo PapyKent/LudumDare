2b1f9426fe808ee3e1230d62fc21a0339bb8117b
jBouland <joris.bouland@gmail.com>
2014-12-06 11:22:00 +0100

    oubli header

---
+++ LudumDare/Header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+const int ECRAN_L = 800;
+const int ECRAN_H = 600;
+
+#endif

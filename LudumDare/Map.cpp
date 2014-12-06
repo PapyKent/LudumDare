1b98a9e5654b45fc9499c2c9704f3dcf32a9cd0b
PapyKent <kentdu38@gmail.com>
2014-12-06 11:10:30 +0100

    ajout map

---
+++ LudumDare/Map.cpp
@@ -0,0 +1,15 @@
+#include "Map.h"
+
+
+Map::Map(void)
+{
+	level=0;
+
+
+
+}
+
+
+Map::~Map(void)
+{
+}
---
+++ LudumDare/Map.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+class Character;
+using namespace std;
+//ddd
+class Map	
+{
+private:
+	int level;
+	vector <Character> tableCharacters;
+public:
+	Map(void);
+	~Map(void);
+
+
+};
+
